f26f1a6595ac43360eb922fce4acafdbfe43f47d
raghav1245anand <raghavanand034@gmail.com>
2026-08-28 20:47:32 +0530

    refactor(i18n): optimize locale string lookup and caching mechanism

---
+++ src/js/i18n-helpers.js
@@ -0,0 +1,25 @@
+﻿/**
+ * i18n Translation & String Cache Utilities
+ */
+const I18nHelper = {
+    cache: new Map(),
+
+    formatString(template, vars = {}) {
+        return template.replace(/{(\w+)}/g, (match, key) => {
+            return typeof vars[key] !== 'undefined' ? vars[key] : match;
+        });
+    },
+
+    getDirection(langCode) {
+        const rtlLangs = ['ur', 'ar'];
+        return rtlLangs.includes(langCode) ? 'rtl' : 'ltr';
+    },
+
+    sanitizeLocale(locale) {
+        return (locale || 'en').toLowerCase().split('-')[0];
+    }
+};
+
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = I18nHelper;
+}
