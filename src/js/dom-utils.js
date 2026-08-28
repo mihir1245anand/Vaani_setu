58c6f62341e6bd1d73b7db5a92cb486dd68fd6e7
raghav1245anand <raghavanand034@gmail.com>
2026-08-28 21:32:41 +0530

    perf(dom): optimize DOM queries and debounce voice input processing

---
+++ src/js/dom-utils.js
@@ -0,0 +1,33 @@
+﻿/**
+ * High Performance DOM & Event Utility Functions
+ */
+const DOMUtils = {
+    debounce(func, wait = 250) {
+        let timeout;
+        return function(...args) {
+            clearTimeout(timeout);
+            timeout = setTimeout(() => func.apply(this, args), wait);
+        };
+    },
+
+    throttle(func, limit = 300) {
+        let inThrottle;
+        return function(...args) {
+            if (!inThrottle) {
+                func.apply(this, args);
+                inThrottle = true;
+                setTimeout(() => inThrottle = false, limit);
+            }
+        };
+    },
+
+    escapeHTML(str) {
+        const div = document.createElement('div');
+        div.textContent = str;
+        return div.innerHTML;
+    }
+};
+
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = DOMUtils;
+}
