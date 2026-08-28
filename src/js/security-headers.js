499eb14cae9477544b38ba67e177eb0aa043e3db
raghav1245anand <raghavanand034@gmail.com>
2026-08-28 22:20:45 +0530

    refactor(server): add rate limiting and security headers middleware in server.js

---
+++ src/js/security-headers.js
@@ -0,0 +1,12 @@
+﻿/**
+ * Express / Node HTTP Security Headers Utility
+ */
+function applySecurityHeaders(req, res, next) {
+    res.setHeader('X-Content-Type-Options', 'nosniff');
+    res.setHeader('X-Frame-Options', 'SAMEORIGIN');
+    res.setHeader('X-XSS-Protection', '1; mode=block');
+    res.setHeader('Referrer-Policy', 'strict-origin-when-cross-origin');
+    if (next) next();
+}
+
+module.exports = applySecurityHeaders;
