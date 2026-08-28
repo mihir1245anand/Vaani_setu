b87f76509046380cfe9f285949b6d7db62460e97
raghav1245anand <raghavanand034@gmail.com>
2026-08-28 22:44:15 +0530

    feat(utils): add date formatting and currency formatting localization helpers

---
+++ src/js/formatters.js
@@ -0,0 +1,20 @@
+﻿/**
+ * Regional Currency & Number Localization Formatters
+ */
+const Formatters = {
+    formatINR(amount) {
+        return new Intl.NumberFormat('en-IN', {
+            style: 'currency',
+            currency: 'INR',
+            maximumFractionDigits: 0
+        }).format(amount);
+    },
+
+    formatIndianNumber(num) {
+        return new Intl.NumberFormat('en-IN').format(num);
+    }
+};
+
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = Formatters;
+}
